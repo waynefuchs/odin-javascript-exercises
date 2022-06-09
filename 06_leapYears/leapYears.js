060ca0b8b82afd8c873ed4fc5f39b665b1f10ff4
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-06-08 23:00:12 -0400

    Implemented leapYears
    All tests pass

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(inputYear) {
+    if(inputYear % 100 == 0 && inputYear % 400 == 0) return true;
+    if(inputYear % 100 == 0) return false;
+    if(inputYear % 4 == 0) return true;
+    return false;
 };
 
 // Do not edit below this line
