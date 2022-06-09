7e8b744af7f4ba7a150e2095bc1acfbcb6f88dbb
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-06-08 22:55:44 -0400

    Implement sumAll
    Passes all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
+    if(numA < 0 
+        || numB < 0
+        || typeof(numA) != "number" 
+        || typeof(numB) != "number")
+        return "ERROR";
 
+    const min = Math.min(numA, numB);
+    const max = Math.max(numA, numB);
+
+    let sum = 0;
+    for(let i=min; i<=max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
