98927dac21360a578a6061b8cc150407815ce8b9
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-06-08 22:19:25 -0400

    Add function repeatString
    All tests pass.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, repeatCount) {
+    if(repeatCount < 0) return "ERROR";
+    return inputString.repeat(repeatCount);
 };
 
 // Do not edit below this line
