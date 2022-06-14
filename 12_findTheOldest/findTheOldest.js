9e238e604d70770a87491555d739dba1591c3f8b
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-06-13 22:50:10 -0400

    Implement findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = (people) =>
+    people.reduce((oldestPerson, currentPerson) => 
+        getAge(oldestPerson) < getAge(currentPerson) 
+            ? currentPerson 
+            : oldestPerson);
 
-};
+function getAge(person)
+{
+    if(typeof(person) !== "object") return 0;
+    return (
+            ('yearOfDeath' in person) 
+                ? person.yearOfDeath 
+                : (new Date()).getFullYear()
+        ) - person.yearOfBirth;
+}
 
+        
 // Do not edit below this line
 module.exports = findTheOldest;
