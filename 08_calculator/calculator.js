8519f769c87b44e76dbc35dfac849d1fbc99f212
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-06-13 20:58:05 -0400

    Complete implementation

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,9 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const sum = (input) => input.reduce((total, current) => total + current, 0);
+const multiply = (input) => input.reduce((total, current) => total * current, 1);
+const power = (a, b) => a ** b;
+const factorial = (n) => (Array.from(new Array(n), (x, i) => i+1)).reduce((total, current) => total * current, 1);
 
 // Do not edit below this line
 module.exports = {
