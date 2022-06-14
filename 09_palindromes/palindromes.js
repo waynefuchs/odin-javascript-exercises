55b756c126dcaeff31537e155faa51b972e57618
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-06-13 21:45:30 -0400

    Implement palindrome check

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let zeroPunctuation = input.replace(/[^a-z]/gi, '').toLowerCase();
+    let zeroPunctuationReverse = zeroPunctuation.split("").reverse().join("");
+    return zeroPunctuation === zeroPunctuationReverse;
 };
 
 // Do not edit below this line
