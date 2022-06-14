83cfad29b21447266958df180fb51b7d1330f8e2
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-06-13 22:02:39 -0400

    Implement fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+let fibonacciCache = {};
 
+const fibonacci = function(n) {
+    // allow string
+    n = Number(n);
+    if(n < 0) return "OOPS";
+    if(n <=1) return n;
+    // the cache provides significant speed-up
+    if(n in fibonacciCache) return fibonacciCache[n];
+    let result = fibonacci(n-1) + fibonacci(n-2);
+    fibonacciCache[n] = result;
+    return result;
 };
 
 // Do not edit below this line
